448e6ac1c43d6ce8b6a605ab090d572b5cf81159
Jay Morris <misc@mjya.uk>
2020-10-27 13:44:33 +0000

    Modularised code by moving command line processing function to new file in MPAGSCipher

---
+++ src/MPAGSCipher/ProcessCommandLine.hpp
@@ -0,0 +1,16 @@
+#ifndef MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+#define MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
+
+#include <vector>
+#include <string>
+#include <cctype>
+#include <iostream>
+
+//! Process the command line arguments
+bool processCommandLine(
+  const std::vector<std::string>& cmdLineArgs,
+  bool& helpRequested,
+  bool& versionRequested,
+  std::string& inputFile,
+  std::string& outputFile );
+#endif // MPAGSCIPHER_PROCESSCOMMANDLINE_HPP
--- src/mpags-cipher.cpp
+++ src/mpags-cipher.cpp
@@ -9,66 +9,7 @@
 
 // Our project headers
 #include "TransformChar.hpp"
-
-// process the command line arguments
-bool processCommandLine(
-  const std::vector<std::string>& cmdLineArgs,
-  bool& helpRequested,
-  bool& versionRequested,
-  std::string& inputFile,
-  std::string& outputFile ) 
-{
-  // Add a typedef that assigns another name for the given type for clarity
-  typedef std::vector<std::string>::size_type size_type;
-  const size_type nCmdLineArgs {cmdLineArgs.size()};
-
-  // Process command line arguments - ignore zeroth element, as we know this to
-  // be the program name and don't need to worry about it
-  for (size_type i {1}; i < nCmdLineArgs; ++i) {
-
-    if (cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help") {
-      helpRequested = true;
-    }
-    else if (cmdLineArgs[i] == "--version") {
-      versionRequested = true;
-    }
-    else if (cmdLineArgs[i] == "-i") {
-      // Handle input file option
-      // Next element is filename unless -i is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -i requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	inputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else if (cmdLineArgs[i] == "-o") {
-      // Handle output file option
-      // Next element is filename unless -o is the last argument
-      if (i == nCmdLineArgs-1) {
-	std::cerr << "[error] -o requires a filename argument" << std::endl;
-	// exit main with non-zero return to indicate failure
-	return 1;
-      }
-      else {
-	// Got filename, so assign value and advance past it
-	outputFile = cmdLineArgs[i+1];
-	++i;
-      }
-    }
-    else {
-      // Have an unknown flag to output error message and return non-zero
-      // exit status to indicate failure
-      std::cerr << "[error] unknown argument '" << cmdLineArgs[i] << "'\n";
-      return 1;
-    }
-  }
-return 0;
-}
+#include "ProcessCommandLine.hpp"
 
 // Main function of the mpags-cipher program
 int main(int argc, char* argv[])
@@ -118,27 +59,56 @@ int main(int argc, char* argv[])
   // Read in user input from stdin/file
   // Warn that input file option not yet implemented
   if (!inputFile.empty()) {
-    std::cout << "[warning] input from file ('"
+
+    std::ifstream in_file{inputFile};
+    bool okay_to_read = in_file.good();
+
+    if (okay_to_read){
+      while(in_file >> inputChar){
+        inputText += transformChar(inputChar);
+      }
+    }
+    else {
+      std::cout << "[warning] input file ('"
               << inputFile
-              << "') not implemented yet, using stdin\n";
+              << "') could not be opened\n";
+      return 1;
+    }
+  }
+  else {
+        // Loop over each character from user input
+    // (until Return then CTRL-D (EOF) pressed)
+    std::cout << "[warning] no input file given, using cin. Press CTRL-D to run after entering input.\n" << std::endl;
+    while(std::cin >> inputChar)
+    { 
+      inputText += transformChar( inputChar );
+    }
   }
 
-  // Loop over each character from user input
-  // (until Return then CTRL-D (EOF) pressed)
-  while(std::cin >> inputChar)
-  { 
-    inputText += transformChar( inputChar );
-  }
+
 
   // Output the transliterated text
   // Warn that output file option not yet implemented
   if (!outputFile.empty()) {
-    std::cout << "[warning] output to file ('"
-              << outputFile
-              << "') not implemented yet, using stdout\n";
-  }
+    std::ofstream out_file {outputFile};
+    bool okay_to_write = out_file.good();
 
-  std::cout << inputText << std::endl;
+    if (okay_to_write){
+      out_file << inputText;
+    }
+    else
+    {
+      std::cout << "[warning] output file ('"
+              << outputFile
+              << "') could not be opened\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cout << "[warning] no output file given, using stdout\n" << std::endl;
+    std::cout << inputText << std::endl;
+  }
 
   // No requirement to return from main, but we do so for clarity
   // and for consistency with other functions
